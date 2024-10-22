a7e52c9b3d434bb33ded9e25fa9a2caf85a97de7
TyrekBrunson <tyrekbrunson@aol.com>
2024-10-22 17:36:49 -0400

    meaningful message

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import App from "./App"; // Import the App component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <p> Hi World! </p>
+    <App />
   </React.StrictMode>
 );
 
